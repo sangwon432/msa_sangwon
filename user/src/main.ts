5d14342c813c520015e1dba3c4d0d87f48f231f1
sangwon432 <swonl0622@gmail.com>
2024-07-19 20:30:13 -0700

    microservice #1

--- user/src/main.ts
+++ user/src/main.ts
@@ -1,8 +1,46 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {RmqOptions, Transport} from "@nestjs/microservices"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<RmqOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [`amqp://rabbitmq:5672`],
+      queue: 'create_charge_psp',
+      prefetchCount: 1,
+      persistent: true,
+      noAck: false,
+      queueOptions: {
+        durable: true,
+      },
+      socketOptions: {
+        heartbeatIntervalInSeconds: 60,
+        reconnectTimeInSeconds: 5,
+      },
+    },
+  // app.connectMicroservice<RmqOptions>({
+  //   transport: Transport.RMQ,
+  //   options: {
+  //     urls: [`amqp://rabbitmq:5672`],
+  //     // urls: [`amqp://admin:password@rabbitmq:5672`],
+  //     queue: 'test_sangwon',
+  //     prefetchCount: 1,
+  //     persistent: true,
+  //     noAck: false,
+  //     queueOptions: {
+  //       durable: true,
+  //     },
+  //     socketOptions: {
+  //       heartbeatIntervalInSeconds: 60,
+  //       reconnectTimeInSeconds: 5,
+  //     }
+  //   }
+  })
+
+  await app.startAllMicroservices()
   await app.listen(3000);
 }
 bootstrap();
